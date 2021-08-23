04d002d0b92389ee6ad3001386515a8c0f17ed90
laibing sun <sunlaibing88@gmail.com>
2021-08-24 07:46:40 +0800

    3_5 done, last is 3_4

--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -8,6 +8,7 @@
 
 class UBoxComponent;
 class UDecalComponent;
+class USoundBase;
 
 UCLASS()
 class FPSGAME_API AFPSExtractionZone : public AActor
@@ -29,4 +30,6 @@ protected:
 	UFUNCTION()
 	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UPROPERTY(EditDefaultsOnly, Category="Sounds")
+	USoundBase* ObjectiveMissingSound;
 };
