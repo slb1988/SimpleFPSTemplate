642d3e334f1f25c575a5d7eb23310a17843d138d
laibing sun <sunlaibing88@gmail.com>
2021-08-24 08:19:14 +0800

    3_6_2 c++ version

--- Source/FPSGame/Private/FPSGameMode.cpp
+++ Source/FPSGame/Private/FPSGameMode.cpp
@@ -4,7 +4,7 @@
 #include "FPSHUD.h"
 #include "FPSCharacter.h"
 #include "UObject/ConstructorHelpers.h"
-
+#include "Kismet/GameplayStatics.h"
 
 AFPSGameMode::AFPSGameMode()
 {
@@ -21,6 +21,28 @@ void AFPSGameMode::CompleteMission(APawn* InstigaterPawn)
 	if (InstigaterPawn)
 	{
 		InstigaterPawn->DisableInput(nullptr);
+
+		if (SpectatingViewpointClass)
+		{
+			TArray<AActor*> ReturnedActors;
+			UGameplayStatics::GetAllActorsOfClass(this, SpectatingViewpointClass, ReturnedActors);
+
+			// Change view target if any valid actor found.
+			if (ReturnedActors.Num() > 0)
+			{
+				AActor* NewViewTarget = ReturnedActors[0];
+
+				APlayerController* PC = Cast<APlayerController>(InstigaterPawn->GetController());
+				if (PC)
+				{
+					PC->SetViewTargetWithBlend(NewViewTarget, 0.5f, EViewTargetBlendFunction::VTBlend_Cubic);
+				}
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("SpectatingViewpoint is nullptr"));
+		}
 	}
 
 	OnMissionCompleted(InstigaterPawn);
